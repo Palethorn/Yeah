52c017dc06c394fcb3fef0750ef5e906a0bf1079
David Cavar <wizzard405@gmail.com>
2015-02-24 19:07:12 +0100

    Simple route test using mock objects

--- tests/unit/SimpleRouteTest.php
+++ tests/unit/SimpleRouteTest.php
@@ -3,8 +3,6 @@
 class SimpleRouteTest extends PHPUnit_Framework_TestCase {
 
     public function __construct() {
-        $this->request = new Yeah\Fw\Http\Request();
-        $this->request->setRequestUri('/');
         $this->route = array(
             'pattern' => '/',
             'secure' => false,
@@ -23,38 +21,45 @@ class SimpleRouteTest extends PHPUnit_Framework_TestCase {
         })
             )
         );
+        $this->request = $this->getMockBuilder('\Yeah\Fw\Http\Request')->getMock();
+        $this->request->method('getRequestUri')->willReturn('/');
+        
+        $this->response = $this->getMockBuilder('\Yeah\Fw\Http\Response')->getMock();
+        $this->session = $this->getMockBuilder('\Yeah\Fw\Session\NullSessionHandler')->getMock();
+        $this->auth = $this->getMockBuilder('\Yeah\Fw\Auth\NullAuth')->getMock();
+        
     }
 
     public function testGet() {
-        $this->request->setRequestMethod('GET');
+        $this->request->method('getRequestMethod')->willReturn('GET');
         $routeRequestHandler = new Yeah\Fw\Routing\RouteRequest\SimpleRouteRequestHandler();
         $route = $routeRequestHandler->handle($this->route, $this->request);
         $this->assertNotFalse($route);
-        $this->assertEquals('GET', $route->execute($this->request, new \Yeah\Fw\Http\Response(), new \Yeah\Fw\Session\NullSessionHandler(), new Yeah\Fw\Auth\NullAuth()));
+        $this->assertEquals('GET', $route->execute($this->request, $this->response, $this->session, $this->auth));
     }
 
     public function testPost() {
-        $this->request->setRequestMethod('POST');
+        $this->request->method('getRequestMethod')->willReturn('POST');
         $routeRequestHandler = new Yeah\Fw\Routing\RouteRequest\SimpleRouteRequestHandler();
         $route = $routeRequestHandler->handle($this->route, $this->request);
         $this->assertNotFalse($route);
-        $this->assertEquals('POST', $route->execute($this->request, new \Yeah\Fw\Http\Response(), new \Yeah\Fw\Session\NullSessionHandler(), new Yeah\Fw\Auth\NullAuth()));
+        $this->assertEquals('POST', $route->execute($this->request, $this->response, $this->session, $this->auth));
     }
 
     public function testPut() {
-        $this->request->setRequestMethod('PUT');
+        $this->request->method('getRequestMethod')->willReturn('PUT');
         $routeRequestHandler = new Yeah\Fw\Routing\RouteRequest\SimpleRouteRequestHandler();
         $route = $routeRequestHandler->handle($this->route, $this->request);
         $this->assertNotFalse($route);
-        $this->assertEquals('PUT', $route->execute($this->request, new \Yeah\Fw\Http\Response(), new \Yeah\Fw\Session\NullSessionHandler(), new Yeah\Fw\Auth\NullAuth()));
+        $this->assertEquals('PUT', $route->execute($this->request, $this->response, $this->session, $this->auth));
     }
 
     public function testDelete() {
-        $this->request->setRequestMethod('DELETE');
+        $this->request->method('getRequestMethod')->willReturn('DELETE');
         $routeRequestHandler = new Yeah\Fw\Routing\RouteRequest\SimpleRouteRequestHandler();
         $route = $routeRequestHandler->handle($this->route, $this->request);
         $this->assertNotFalse($route);
-        $this->assertEquals('DELETE', $route->execute($this->request, new \Yeah\Fw\Http\Response(), new \Yeah\Fw\Session\NullSessionHandler(), new Yeah\Fw\Auth\NullAuth()));
+        $this->assertEquals('DELETE', $route->execute($this->request, $this->response, $this->session, $this->auth));
     }
 
 }
