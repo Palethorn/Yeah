139590ead616d0a187d32c571e773712757efd09
David Cavar <wizzard405@yahoo.com>
2016-10-16 00:10:34 +0200

    Tests autoloader issues

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -46,6 +46,6 @@ $_GET['lol'] = 10;
 require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Yeah' . DIRECTORY_SEPARATOR . 'Fw' . DIRECTORY_SEPARATOR . 'Application' . DIRECTORY_SEPARATOR . 'Autoloader.php';
 
 $autoloader = new Yeah\Fw\Application\Autoloader();
-$autoloader->addIncludePath('..' . DIRECTORY_SEPARATOR . '..');
+$autoloader->addIncludePath(dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..');
 $autoloader->register();
 $autoloader->setCache(new \Yeah\Fw\Cache\NullCache());
