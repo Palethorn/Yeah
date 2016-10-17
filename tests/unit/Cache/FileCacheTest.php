d5a67a1df151002a2feb42994e666f967a8208b7
David Cavar <wizzard405@gmail.com>
2016-10-17 18:54:12 +0200

    Added FileCacheTest

---
+++ tests/unit/Cache/FileCacheTest.php
@@ -0,0 +1,34 @@
+<?php
+
+class DatabaseAuthTest extends PHPUnit_Framework_TestCase {
+
+    public function __construct() {
+        $this->file_cache = new \Yeah\Fw\Cache\FileCache('/tmp/test');
+        $this->file_cache->set('test', 'test');
+    }
+
+    public function testGet() {
+        $this->assertEquals($this->file_cache->get('test'), 'test');
+    }
+
+    public function testReinitializedGet() {
+        $file_cache = new \Yeah\Fw\Cache\FileCache('/tmp/test');
+        $this->assertEquals($file_cache->get('test'), 'test');
+    }
+
+    public function testHas() {
+        $this->assertTrue($this->file_cache->has('test'));
+    }
+
+    public function testReinitializedHas() {
+        $file_cache = new \Yeah\Fw\Cache\FileCache('/tmp/test');
+        $this->assertTrue($file_cache->has('test'));
+    }
+
+    public function testRemove() {
+        $this->file_cache->remove('test');
+        $this->assertFalse($this->file_cache->has('test'));
+        $this->assertNull($this->file_cache->get('test'));
+        $this->file_cache->set('test', 'test');
+    }
+}
