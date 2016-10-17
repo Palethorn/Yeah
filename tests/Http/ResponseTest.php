183db682faeb8ea1542d0e382032b47b5ee80303
David Cavar <wizzard405@gmail.com>
2016-10-17 19:33:40 +0200

    Moved tests

---
+++ tests/Http/ResponseTest.php
@@ -0,0 +1,20 @@
+<?php
+
+class ResponseTest extends PHPUnit_Framework_TestCase {
+    public function testResponseHeadering() {
+        $response = new \Yeah\Fw\Http\Response();
+        $response->setHeader('Test-Header', 'test');
+        $response->setHeaders(array(
+            'H1' => '1',
+            'H2' => '2'
+        ));
+        
+        $this->assertEquals($response->getHeader('Test-Header'), 'test');
+        $this->assertEquals($response->getHeader('H1'), '1');
+        $this->assertEquals($response->getHeader('H2'), '2');
+        $this->assertTrue($response->hasHeader('Test-Header'));
+        $this->assertTrue($response->hasHeader('H1'));
+        $this->assertTrue($response->hasHeader('H2'));
+        $this->assertFalse($response->hasHeader('No-Header'));
+    }
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,51 @@
+<?php
+error_reporting(E_ALL ^ E_NOTICE);
+$_SERVER = array(
+    'PHP_SELF' => '1',
+    'GATEWAY_INTERFACE' => '2',
+    'SERVER_ADDR' => '3',
+    'SERVER_NAME' => '4',
+    'SERVER_SOFTWARE' => '5',
+    'SERVER_PROTOCOL' => '6',
+    'REQUEST_METHOD' => '7',
+    'REQUEST_TIME' => '8',
+    'REQUEST_TIME_FLOAT' => '9',
+    'QUERY_STRING' => '10',
+    'DOCUMENT_ROOT' => '11',
+    'HTTP_ACCEPT' => '12',
+    'HTTP_ACCEPT_CHARSET' => '13',
+    'HTTP_ACCEPT_ENCODING' => '14',
+    'HTTP_ACCEPT_LANGUAGE' => '15',
+    'HTTP_CONNECTION' => '16',
+    'HTTP_HOST' => '17',
+    'HTTP_REFERER' => '18',
+    'HTTP_USER_AGENT' => '19',
+    'HTTPS' => '20',
+    'REMOTE_ADDR' => '21',
+    'REMOTE_HOST' => '22',
+    'REMOTE_PORT' => '23',
+    'REMOTE_USER' => '24',
+    'REDIRECT_REMOTE_USER' => '25',
+    'SCRIPT_FILENAME' => '26',
+    'SERVER_ADMIN' => '27',
+    'SERVER_PORT' => '28',
+    'SERVER_SIGNATURE' => '29',
+    'PATH_TRANSLATED' => '30',
+    'SCRIPT_NAME' => '31',
+    'REQUEST_URI' => '32',
+    'PHP_AUTH_DIGEST' => '33',
+    'PHP_AUTH_USER' => '34',
+    'PHP_AUTH_PW' => '35',
+    'AUTH_TYPE' => '36',
+    'PATH_INFO' => '37',
+    'ORIG_PATH_INFO' => '38'
+);
+
+$_GET['lol'] = 10;
+
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Yeah' . DIRECTORY_SEPARATOR . 'Fw' . DIRECTORY_SEPARATOR . 'Application' . DIRECTORY_SEPARATOR . 'Autoloader.php';
+
+$autoloader = new Yeah\Fw\Application\Autoloader();
+$autoloader->addIncludePath(dirname(__FILE__) . DIRECTORY_SEPARATOR . '..');
+$autoloader->register();
+$autoloader->setCache(new \Yeah\Fw\Cache\NullCache());
